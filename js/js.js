35f4eeb5f037e32ad6d7be8e36fde49e39839f45
srgg6701 <srgg67@gmail.com>
2014-03-15 22:38:35 +0400

    Local handlers for the table are set.

--- js/js.js
+++ js/js.js
@@ -1,22 +1,80 @@
-$(function(){
-    var btn_add_record =$('#add_record'); 
+$(function() {
+    var tblAdressBook = getAddressBook();
+    var lastTr = $('tr:last-child', tblAdressBook);
+    // set td width as HTML-attribute to be sure that it is fixed and will not be changed
+    $(lastTr).prev().find('td').each(function(index,element){
+        var tdWidth = setInputWidth(element,'width')
+        $(element).attr('width',tdWidth);                
+    });   
+    // penultimate cols - set align center
+    $('tr td:last-child', tblAdressBook).prev().css('text-align', 'center');
+    // the buttons
+    var btn_add_record = $('#add_record');
+    // manage btn text
     $(btn_add_record).text('Add record');
     var add_record_val_add = $(btn_add_record).text();
     var add_record_val_cancel = 'Cancel';
-    $(btn_add_record).on('click', function(){
-        var lastTr = $('#tblAddressBook tr:last-child');       
-        $('td',$(lastTr).prev()).each(function(index,element){
+    // add new record (show next tr)
+    $(btn_add_record).on('click', function() {
+        $('td', $(lastTr).prev()).each(function(index, element) {
             // align the width of inputs and tds (width 100% is not the case :()
-            $('td input[type="text"]',lastTr)
-                .eq($(element).index())
-                    .css('width', parseInt($(element).css('width'))-1-1+'px');            
+            $('td input[type="text"]', lastTr)
+                    .eq($(element).index())
+                    .css('width', setInputWidth(element, 'width', true));
         });
         // manage button text
-        $(lastTr).toggle(400, function(){
-            var btnVal = ($(this).is(':visible'))?
-                                add_record_val_cancel:add_record_val_add;
-            console.log('btnVal: '+btnVal);
+        $(lastTr).toggle(400, function() {
+            var btnVal = ($(this).is(':visible')) ?
+                    add_record_val_cancel : add_record_val_add;
             $(btn_add_record).text(btnVal);
         });
-    })
+    });
+    // make cell text editable
+    $('button.edit, button.save:not(.add)').on('click', function() {
+        handleCell(this);
+    });
+    // remove record
+    $('tr td:last-child div', tblAdressBook).on('click', function(){
+        $(this).parents('tr').eq(0).fadeOut(400,function(){
+            console.log('deleting record from local storage...');
+        });
+    });
 });
+function getAddressBook(){
+    return $('#tblAddressBook');
+}
+// turn td content into input and vice versa
+function handleCell(btn){
+btn.className=(btn.className=='save')? 'edit':'save';    
+$(btn).parents('tr').eq(0).find('td')
+    .each(function(index, element) {
+        if ($(element).has('button').size()) {
+            return false;
+        } else {
+            if(btn.className=='save'){
+                // store td text
+                var cellText = $(element).text();
+                // clean td and set width as HTML-attribute to be sure that it is fixed
+                $(element).text('');
+                // create cell
+                var input = $('<input/>',{
+                        value:cellText
+                    }).css('width', $(element).attr('width')+'px');
+                // set padding params for editable cell
+                $(element).html(input);
+            }else{
+                var cellText = $('input',element).val();
+                $(element).text(cellText);
+                $('input',element).remove();
+            }
+        }
+    });
+}
+// get th width as base param for calculations
+function setInputWidth(element, wType, px, wMinus) {
+    if(!wMinus) wMinus = 0.5; //console.log(wType+' : '+$(element).innerWidth());
+    var eIndex=$(element).index();
+    var w=$('tbody tr:first-child th',getAddressBook()).eq(eIndex)[wType]()- wMinus; //console.log('w : '+w)
+    if(px) w+='px';
+    return  w; 
+} 
