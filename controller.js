ced63c3fdcc06915de40f6e80b5b235a23c72ec3
srgg6701 <srgg67@gmail.com>
2014-03-16 12:48:20 +0400

    The Angular handler prototype set.

--- controller.js
+++ controller.js
@@ -1,6 +1,23 @@
 function abController($scope){
-    $.scope.contacts = [];
-    $.scope.addContact = function(){
-        
+    $scope.contacts = [
+        { name: "Anton", 
+          surname: "Cruseiro", 
+          phone_number: "+12563214785", 
+          group: "Designer" 
+        },
+    ];
+    $scope.addContact = function(){
+        if($scope.name && $scope.phone_number){
+            $scope.contacts.push({
+                name:           $scope.name, 
+                surname:        $scope.surname, 
+                phone_number:   $scope.phone_number, 
+                group:          $scope.group
+            });
+            $scope.name = '';
+            $scope.surname = '';
+            $scope.phone_number = '';
+            $scope.group = '';
+        }
     }
 }
