dabceac5244e769ad6e7713e53932050744efc50
srgg6701 <srgg67@gmail.com>
2014-03-16 18:35:01 +0400

    inputs widths handler is modified.

--- controller.js
+++ controller.js
@@ -10,6 +10,20 @@ function abController($scope){
             phone_number:   storage['phone_number'], 
             group:          storage['group']
         });
+        var groups=[];
+        for(var i in $scope.contacts){ //console.log('contacts:');console.dir($scope.contacts[i]);
+            var current_group = $scope.contacts[i].group;
+            if(!groups.length) {
+                // initialize an array:
+                groups[0]=current_group;
+            }else{
+                // add new group to the array:
+                if($.inArray(current_group, groups)==-1){ 
+                    groups.push(current_group);
+                }
+            }
+            groups.sort();
+        }   console.log('%cgroups:', 'color:blue');console.dir(groups);
     }
     // fill table from localStorage:
     for(var contact in adressBook){
@@ -32,6 +46,7 @@ function abController($scope){
         if($(event.currentTarget).hasClass('add')){           
             $scope.addContact();
         }
+        handleCell(event.currentTarget);
     }
     // remove a record from the local data and store renewed data in the localStorage: 
     $scope.removeRecord = function(event){
@@ -46,5 +61,34 @@ function abController($scope){
     var storeData = function(){
         console.log('store the record in DB...');
         window.localStorage.setItem('adress_book', JSON.stringify($scope.contacts));
+    }
+    
+    // turn td content into input and vice versa
+    function handleCell(btn){ //console.log('btn: '); console.dir(btn);
+    btn.className=(btn.className=='save')? 'edit':'save';    
+    var THs =$(btn).parents('table').eq(0).find('th');  
+    $(btn).parents('tr').eq(0).find('td')
+        .each(function(index, element) {
+            if ($(element).has('button').size()) {
+                return false;
+            } else {
+                if(btn.className=='save'){
+                    // store td text
+                    var cellText = $(element).text();
+                    // clean td and set width as HTML-attribute to be sure that it is fixed
+                    $(element).text('');
+                    // create cell
+                    var input = $('<input/>',{
+                            value:cellText
+                        }).css('width', $(THs).eq(index).attr('width')+'px');
+                    // set padding params for editable cell
+                    $(element).html(input);
+                }else{
+                    var cellText = $('input',element).val();
+                    $(element).text(cellText);
+                    $('input',element).remove();
+                }
+            }
+        });
     }
 }
