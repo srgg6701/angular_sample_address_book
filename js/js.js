154bd2d35344e21da3841e1ab9fef170de18d091
srgg6701 <srgg67@gmail.com>
2014-03-17 01:04:36 +0400

    Inner rows visibility switcher is set.

--- js/js.js
+++ js/js.js
@@ -1,20 +1,35 @@
+var AddressBook;
 $(function() {
+    AddressBook={
+        getTable:function(){
+            return $('#tblAddressBook');
+        },
+        getLastTr:function(){
+            return $('tr:last-child', this.table);
+        },
+        getBtn:function(){
+            return $('#add_record');
+        },
+        setBtnValDefault:function(){
+            $(this.getBtn()).text('Add record');
+        },
+        setBtnValCancel:function(){
+            $(this.getBtn()).text('Cancel');
+        }
+    }
     /*  set TH width as HTML-attribute to be sure that it is fixed 
         and will not be changed in future */
-    $('tbody tr:first-child th',getAddressBook())
+    $('tbody tr:first-child th',AddressBook.getTable())//getAddressBook()
         .each(function(index,element){
             $(element).attr('width', $(element).width());
     });   
-    // get the buttons
-    var btn_add_record = $('#add_record');
-    // manage btn texts
-    $(btn_add_record).text('Add record');
-    var add_record_val_add = $(btn_add_record).text();
-    var add_record_val_cancel = 'Cancel';
-    // add new record (show next tr)
-    $(btn_add_record).on('click', function() {
+    // set default value on the button under the table:
+    AddressBook.setBtnValDefault();
+    // add new record (show next tr):
+    $(AddressBook.getBtn()).on('click', function() {
+        var btn_add_record = this;   
         // get the last row:
-        var lastTr = getLastTr();   
+        var lastTr = AddressBook.getLastTr();
         $('td', $(lastTr).prev()).each(function(index, element) {
             // align the width of inputs and tds (width 100% is not the case :()
             $('td input[type="text"]', lastTr)
@@ -23,25 +38,46 @@ $(function() {
         });
         // manage button text
         $(lastTr).toggle(400, function() {
-            var btnVal = ($(this).is(':visible')) ?
-                    add_record_val_cancel : add_record_val_add;
-            $(btn_add_record).text(btnVal);
+            $(btn_add_record).text( ($(this).is(':visible')) ?
+                    AddressBook.setBtnValCancel() : AddressBook.setBtnValDefault() );
         });
     });
+    $('#gr').on('click', function(){
+        if(!$(this).attr('data-collapsed')){
+            console.dir(groups);
+            var Table=AddressBook.getTable();
+            var Ths=$('tr:has(th)',Table);
+            $('tr:not(:has(th))',Table).hide();
+            for(var i in groups){
+                var tr=$('<tr/>',{
+                    class:'group',
+                    'data-group':groups[i]
+                });
+                var td=$('<td/>',{
+                    colspan:6,
+                    //onclick:alert('hello')
+                }).text(groups[i]);
+                $(tr).append(td)
+                $(Ths).after(tr);            
+            }
+            $(this).attr('data-collapsed', '1');
+        }
+    });
+    $(AddressBook.getTable()).on('click','tr.group', function(event){
+        //alert('clicked');
+        console.dir(Contacts);
+        var Table=AddressBook.getTable();
+        var Tr=event.currentTarget;
+        var dataGroup = $(Tr).attr('data-group');
+        var trGroup = $('tr:not(.group):has(td:contains("'+dataGroup+'"))',Table);
+        $(Tr).after(trGroup);
+        $(trGroup).toggle();
+    });
 });
-// get the table as jQuery object
-function getAddressBook(){
-    return $('#tblAddressBook');
-}
-// get last row in the table
-function getLastTr(tblAdressBook){
-    if(!tblAdressBook) tblAdressBook=getAddressBook();
-    return $('tr:last-child', tblAdressBook);
-}
 // get th width as base param for calculations
-function setInputWidth(element, /*px, */wMinus) {
+function setInputWidth(element, wMinus) {
     if(!wMinus) wMinus = 0.5; //console.log(wType+' : '+$(element).innerWidth());
     var eIndex=$(element).index();
-    var th = $('tbody tr:first-child th',getAddressBook()).eq(eIndex);
+    var th = $('tbody tr:first-child th',AddressBook.getTable()).eq(eIndex);
     return $(th).attr('width')+'px';
 } 
